eef52428c49dae233a40694e72d22c5c27cfe624
tonky0110 <tonky0110@gmail.com>
2019-08-05 21:29:10 +0900

    1.32 Email Sigin Graphql

---
+++ src/api/User/EmailSignIn/EmailSignIn.resolvers.ts
@@ -0,0 +1,29 @@
+import { Resolvers } from 'src/types/resolvers';
+import { EmailSignInResponse, EmailSignInMutationArgs } from 'src/types/graph';
+import User from 'src/entities/User';
+
+const resolvers: Resolvers = {
+	Mutation: {
+		EmailSignIn: async (_, args: EmailSignInMutationArgs): Promise<EmailSignInResponse> => {
+			const { email, password } = args;
+			try {
+				const user = await User.findOne({ email });
+				if (!user) {
+					return {
+						ok: false,
+						error: 'No User found with that email',
+						token: null
+					};
+				}
+			} catch (error) {
+				return {
+					ok: false,
+					error: error.message,
+					token: null
+				};
+			}
+		}
+	}
+};
+
+export default resolvers;
