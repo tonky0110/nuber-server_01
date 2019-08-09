6a4bbf4d428c53dc4bb3ed5277aa6a9d97a40d11
tonky0110 <tonky0110@gmail.com>
2019-08-09 23:15:55 +0900

    1.45 decodeJWT and return user

---
+++ src/utils/decodeJWT.ts
@@ -0,0 +1,15 @@
+import jwt from 'jsonwebtoken';
+import User from '../entities/User';
+
+const decodeJWT = async (token: string): Promise<User | undefined> => {
+	try {
+		const decode: any = jwt.verify(token, process.env.JWT_TOKEN || '');
+		const { id } = decode;
+		const user = await User.findOne({ id });
+		return user;
+	} catch (error) {
+		return undefined;
+	}
+};
+
+export default decodeJWT;
